82062d0e7ef7593f59127b5b2ac60dfb9ebf34bc
ArthurBarbatodaSilva <barbatui0311@gmail.com>
2024-03-18 21:22:32 -0300

    eventos de escolha

--- Projeto-Tecno/index.js
+++ Projeto-Tecno/index.js
@@ -12,3 +12,25 @@ const createWindow = () => {
 app.whenReady().then(() => {
     createWindow()
 })
+
+function escolherPfx(event) {
+    document.querySelector(".main").style.display = "none";
+    document.getElementById("file").click();
+  
+    if (event.target.files && event.target.files[0]) {
+      const arquivo = event.target.files[0];
+  
+      if (arquivo) {
+        const nome = arquivo.name.substring(0, 25);
+        document.getElementById("nomeArquivo").textContent = nome;
+        document.getElementById("file").classList.add("hidden");
+        document.querySelector(".container").style.display = "flex"; 
+    }
+  }
+  }
+  
+  function fecharArquivo() {
+    document.querySelector(".main").style.display = "grid"; 
+    document.querySelector(".container").style.display = "none";
+    document.getElementById("file").value = "";
+  }
